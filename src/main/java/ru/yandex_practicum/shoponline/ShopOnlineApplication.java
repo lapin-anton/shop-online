b16c07675a5a21d82774ecbf0881f92f5eee6492
a.lapin <a.lapin@cloudcom.ru>
2025-04-12 14:13:59 +0300

    initial application

---
+++ src/main/java/ru/yandex_practicum/shoponline/ShopOnlineApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex_practicum.shoponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex_practicum/shoponline/ShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex_practicum.shoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
