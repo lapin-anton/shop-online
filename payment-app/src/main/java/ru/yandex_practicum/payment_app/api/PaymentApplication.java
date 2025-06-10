31206c99adee03bdaec5101df59eb5b70a81e4ca
a.lapin <a.lapin@cloudcom.ru>
2025-06-10 15:59:25 +0300

    add openAPI generator to payment app

--- payment-app/src/main/java/ru/yandex_practicum/payment_app/api/PaymentApplication.java
+++ payment-app/src/main/java/ru/yandex_practicum/payment_app/api/PaymentApplication.java
@@ -1,4 +1,4 @@
-package ru.yandex_practicum.payment_app;
+package ru.yandex_practicum.payment_app.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
-
 }
